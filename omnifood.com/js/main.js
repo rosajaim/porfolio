e02a97be74164107b2063d04902ba3701dc02da3
jaime <rosajaim86@gmail.com>
2021-01-12 22:48:33 -0700

    added mobile navigation

---
+++ omnifood.com/js/main.js
@@ -0,0 +1,77 @@
+
+/* **** STICKY NAVIGATION ***** */
+$(document).ready(function() {
+
+   $('.js--section__features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+   }, {
+        offset: '60px;' 
+ });
+
+
+ /* **** SCROLL ON BUTTONS ***** */
+$('.js--scroll-to-plans').click(function() {
+    $('html, body').animate({
+        scrollTop: $('.js--section__plans').offset().top}, 
+        1000);
+
+});
+
+$('.js--scroll-to-start').click(function() {
+    $('html, body').animate({
+        scrollTop: $('.js--section__features').offset().top}, 
+        1000);
+});
+
+ /* **** ANIMATIONS ON SCROLL ***** */
+ $('.js--wp-1').waypoint(function(direction){
+     $('.js--wp-1').addClass('animate__animated animate__fadeIn');
+
+ }, {
+     offset: '50%' 
+ });
+
+ $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
+
+ }, {
+     offset: '50%'
+ });
+
+ $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+
+ }, {
+     offset: '50%'
+ });
+
+ $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animate__animated animate__pulse');
+
+ }, {
+     offset: '50%'
+ });
+
+
+ /* ***** Mobile Nav ***** */
+ $('.js--nav-icon').click(function() {
+     var nav = $('.js--main__nav-list');
+     var icon = $('.js--nav-icon i');
+
+     nav.slideToggle(200);
+     if (icon.hasClass('ion-navicon-round')) {
+        icon.addClass('ion-close-round');
+        icon.removeClass('ion-navicon-round');
+     }else {
+        icon.addClass('ion-navicon-round');
+        icon.removeClass('ion-close-round');
+     }
+
+ });
+
+});
+
