0e3e34d367cfe132436c59b864a385c6df921402
jaime <rosajaim86@gmail.com>
2021-02-07 22:31:49 -0700

    optimazed imgs

--- jaimewebdev.com/js/main.js
+++ jaimewebdev.com/js/main.js
@@ -187,17 +187,6 @@ $(function () {
   }).resize();
 });
 
-// SCROLL NAV
-// $('a[href^="#"]').on('click', function (e) {
-//   e.preventDefault();
-//   var target = this.hash;
-//   var $target = $(target);
-//   $('html, body').stop().animate({
-//       'scrollTop': $target.offset().top
-//   }, 900, 'swing', function () {
-//       // window.location.hash = target;
-//   });
-// });
 
 
 
