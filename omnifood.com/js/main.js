9a45c226e0c64dfc69706652ed5f4b4abad73d44
jaime <rosajaim86@gmail.com>
2021-02-11 23:48:53 -0700

    added animations

--- omnifood.com/js/main.js
+++ omnifood.com/js/main.js
@@ -63,11 +63,18 @@ $(document).ready(function () {
     });
 
     $('.js--wp-4').waypoint(function (direction) {
-        $('.js--wp-4').addClass('animate__animated animate__pulse');
+        $('.js--wp-4').addClass('animate__animated animate__fadeInDown');
 
     }, {
         offset: '50%'
     });
+
+    // $('.js--wp-5').waypoint(function (direction) {
+    //     $('.js--wp-5').addClass('animate__animated animate__fadeInDown');
+
+    // }, {
+    //     offset: '50%'
+    // });
 
 
     /* ***** MOBILE NAV ***** */
@@ -87,3 +94,29 @@ $(document).ready(function () {
     });
 
 });
+
+    /* ***** DRIFT CHAT***** */
+    "use strict";
+    !function() {
+      var t = window.driftt = window.drift = window.driftt || [];
+      if (!t.init) {
+        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+        t.factory = function(e) {
+          return function() {
+            var n = Array.prototype.slice.call(arguments);
+            return n.unshift(e), t.push(n), t;
+          };
+        }, t.methods.forEach(function(e) {
+          t[e] = t.factory(e);
+        }), t.load = function(t) {
+          var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+          o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+          var i = document.getElementsByTagName("script")[0];
+          i.parentNode.insertBefore(o, i);
+        };
+      }
+    }();
+    drift.SNIPPET_VERSION = '0.3.1';
+    drift.load('h66fg3d68u8v');
+    
